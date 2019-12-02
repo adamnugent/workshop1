f5ef8f774bcfbead14b2a159b63373fc86aec096
adamnugent <anugent@umw.edu>
2019-12-02 14:09:53 -0500

    changee port to 8080

--- workshop1.js
+++ workshop1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set("port", process.env.NODE_PORT);
+app.set("port", 8080);
 app.use(bodyParser.json({type: "applicaiton/json"}));
 app.use(bodyParser.urlencoded({extended: true}));
 
